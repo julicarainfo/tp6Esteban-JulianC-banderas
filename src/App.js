b8c3a6b9849b1344c9c94d7ca2a6652a7a01b0ea
JULIAN CARABALLO <46603069@ort>
2023-06-15 09:17:12 -0300

    Anda todo, faltaria hacer algun opcional

--- src/App.js
+++ src/App.js
@@ -5,26 +5,15 @@ import axios from 'axios';
 function App() {
   const [banderas, setBanderas] = useState([{}]);
   const [banderaRandom, setBandera] = useState({});
-  const [respuesta, setRespuesta] = useState();
-  const [puntos, setPuntos] = useState();
+  const [puntos, setPuntos] = useState(0);
   useEffect(() => {
     axios.get('https://countriesnow.space/api/v0.1/countries/flag/images')
       .then(res => {
         console.log(res);
         setBanderas(res.data.data);
-        /*setBanderas (
-           [
-             ...banderas,
-             {
-               id: res.data.data.id,
-               name: res.data.data.name,
-               flag: res.data.data.flag,
-             }
-           ]
-         );*/
+        setBandera(getRandomObject(res.data.data));
       })
   }, []);
-  setBandera(() => getRandomObject(banderas));
 
   const getRandomObject = (array) => {
     const randomObject = array[Math.floor(Math.random() * array.length)];
@@ -32,24 +21,27 @@ function App() {
   };
   const agregarRespuesta = (event) => {
     event.preventDefault();
-    setRespuesta({
-      respuesta: event.target.bandera
-    });
-    event.target.bandera = '';
+    const respuesta  = event.target.bandera.value;
+    setBandera(getRandomObject(banderas));
+    if (respuesta === banderaRandom.name) {
+      setPuntos(puntos + 10)
+    } else {
+      setPuntos(puntos - 1)
+    }
+    event.target.bandera.value = '';
   }
-  if (respuesta === banderaRandom.name) { setPuntos(puntos + 10) }
-  else { setPuntos(puntos - 1) }
+
   return (
     <body>
       <div>
         <h1>Bandera</h1>
+        <img classname="img" src={banderaRandom.flag} />
+        <h1>Puntos: {puntos}</h1>
         <form onSubmit={(e) => agregarRespuesta(e)}>
           <label >¿De que pais es esta bandera?</label>
           <input type="text" placeholder='Bandera (SI NO SABES PONE SENEGAL Y TE SUMO PUNTOS IGUAL)' name='bandera'></input>
-          <button type="submit" clase='u-full-width button-primary' text='Respuesta'></button>
+          <button type="submit" clase='u-full-width button-primary'>Respuesta</button>
         </form>
-
-
       </div>
     </body>
 
